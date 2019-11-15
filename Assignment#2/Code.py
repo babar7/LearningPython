b3223f53bb0530aa83fbd1a5e64739bf08f5b891
Babar Shair <Babarshair97@gmail.com>
2019-11-15 22:03:29 +0500

    complete assigment #2

--- Assignment#2/Code.py
+++ Assignment#2/Code.py
@@ -1,40 +1,82 @@
 # 1. Write a program which takes 5 inputs from user for different subject’s
 # marks, total it and generate mark sheet using grades ?
-# 2. Write a program which take input from user and identify that the given
-# number is even or odd?
-# 3. Write a program which print the length of the list?
-# 4. Write a Python program to sum all the numeric items in a list?
-# 5. Write a Python program to get the largest number from a numeric list.
-# 6. Take a list, say for example this one:
-# a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-# and write a program that prints out all the elements of the list that are
-# less than 5.
-
-
-# 1. Write a program which takes 5 inputs from user for different subject’s
-# marks, total it and generate mark sheet using grades ?
 # ==================================START==============================================
 
-print("Please write roundoff number in integer form ")
-english = int(input("Please enter your english marks :: "))
-maths = int(input("Please enter your maths marks :: "))
-computer = int(input("Please enter your computer marks :: "))
-science = int(input("Please enter your science marks :: "))
-physics = int(input("Please enter your physics marks :: "))
+english = float(input("Please enter your english marks :: "))
+maths = float(input("Please enter your maths marks :: "))
+computer = float(input("Please enter your computer marks :: "))
+science = float(input("Please enter your science marks :: "))
+physics = float(input("Please enter your physics marks :: "))
 total = english + maths + computer + science + physics
 percentage = (total/500) * 100
 
-if percentage >= 80 and percentage <= 100:
-    print("Your grade is A` with  ", percentage, "%")
-elif percentage >= 70 and percentage < 80:
-    print("Your grade is A with ", percentage, "%")
-elif percentage >= 60 and percentage < 70:
-    print("Your grade is B with ", percentage, "%")
-elif percentage >= 50 and percentage < 60:
-    print("Your grade is C with ", percentage, "%")
-elif percentage > 0 and percentage < 50:
-    print("Sorry you are fail and your percentage is ", percentage, "%")
+if(percentage > 100 or percentage < 0):
+    print("Please check your input and write again")
 else:
-    print("Wrong input")
+    if percentage >= 80:
+        print("Your grade is A` with  ", percentage, "%")
+    elif percentage >= 70:
+        print("Your grade is A with ", percentage, "%")
+    elif percentage >= 60:
+        print("Your grade is B with ", percentage, "%")
+    elif percentage >= 50:
+        print("Your grade is C with ", percentage, "%")
+    elif percentage >= 40:
+        print("Your grade is D with ", percentage, "%")
+    else:
+        print("Sorry you are fail and your percentage is ", percentage, "%")
 
 # ----------------------------------END-------------------------------------------------
+
+# 2. Write a program which take input from user and identify that the given
+# number is even or odd?
+# ==================================START==============================================
+
+numberToIdentify = int(
+    input("Please enter any integer value to identify the number is even or odd"))
+
+if(numberToIdentify % 2):
+    print("Given number is odd")
+else:
+    print("Given number is even")
+
+# ----------------------------------END-------------------------------------------------
+
+# 3. Write a program which print the length of the list?
+# ==================================START==============================================
+numberList = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 100, 14, 60, 50, 12, 19]
+
+print("Length of the list is :", numberList.__len__())
+
+# ----------------------------------END-------------------------------------------------
+
+# 4. Write a Python program to sum all the numeric items in a list?
+# ==================================START==============================================
+_sum = 0
+for number in numberList:
+    _sum = number
+
+print("Sum of list items is :", _sum)
+
+# ----------------------------------END-------------------------------------------------
+
+# 5. Write a Python program to get the largest number from a numeric list.
+# ==================================START==============================================
+
+_max = 0
+for number in numberList:
+    if(number > _max):
+        _max = number
+print("Max number is  :", _max)
+
+# ----------------------------------END-------------------------------------------------
+
+# 6. Take a list, say for example this one:
+# a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 100, 14, 60, 50, 12, 19]
+# and write a program that prints out all the elements of the list that are
+# less than 5.
+# ==================================START==============================================
+
+for number in numberList:
+    if(number < 5):
+        print("Less then 5:", number)
